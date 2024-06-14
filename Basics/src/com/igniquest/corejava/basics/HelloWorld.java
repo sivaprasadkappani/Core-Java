f10b7012cc087cc1c4ad6976fc5d3bdcbf8a8965
sivaprasadkappani <skappaniyil@gmail.com>
2024-06-14 12:13:58 +0530

    Core-Java : Basics

--- Basics/src/com/igniquest/corejava/basics/HelloWorld.java
+++ Basics/src/com/igniquest/corejava/basics/HelloWorld.java
@@ -1,10 +1,13 @@
 package com.igniquest.corejava.basics;
 
+/*
+ *  Class used to pring Hello world message
+ */
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		 // Prints the text Hello, world to the console
+		 System.out.println("Hello, World");
+ 	}
 
 }
